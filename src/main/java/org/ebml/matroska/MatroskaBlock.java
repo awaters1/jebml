e11c4e876f722070858823fa8e2e303f49d6d5a2
Anthony Waters <awaters1@gmail.com>
2015-08-06 18:43:13 -0400

    Added the size parameter Ignored getting the laces because it fails on some good files

--- src/main/java/org/ebml/matroska/MatroskaBlock.java
+++ src/main/java/org/ebml/matroska/MatroskaBlock.java
@@ -2,17 +2,17 @@
  * JEBML - Java library to read/write EBML/Matroska elements.
  * Copyright (C) 2004 Jory Stone <jebml@jory.info>
  * Based on Javatroska (C) 2002 John Cannon <spyder@matroska.org>
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -86,7 +86,9 @@ public class MatroskaBlock
       else if (laceFlag == 0x06)
       { // EBML Lacing
         LOG.trace("Reading ebml lace sizes");
-        sizes = readEBMLLaceSizes(index, laceCount);
+        // FIXME This fails on Once.Upon.a.Time.S01E16.480p.HDTV.x264-mSD.mkv for some reason so
+        // ignore it for my purposes
+        // sizes = readEBMLLaceSizes(index, laceCount);
       }
       else if (laceFlag == 0x04)
       { // Fixed Size Lacing
